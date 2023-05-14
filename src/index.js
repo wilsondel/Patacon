6e16d80e0c0921e8fe08b350731cfe85b4cf2b9f
wilson <wilson.delgadoher@gmail.com>
2023-05-14 18:23:45 -0500

    base auth

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { PublicClientApplication } from '@azure/msal-browser';
+import { msalConfig } from './auth/authConfig';
+import { MsalProvider } from '@azure/msal-react';
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <MsalProvider instance={msalInstance}>
+        <App />
+    </MsalProvider>
   </React.StrictMode>
 );
 
