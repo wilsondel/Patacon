e7b389a9c5547030fc77676bc55cc27332de572c
wilson <wilson.delgadoher@gmail.com>
2023-05-14 12:18:18 -0500

    Integrated version notes and canvas

--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -2,22 +2,26 @@ import React, { useEffect } from 'react';
 import io from 'socket.io-client';
 import "./style.css"
 
+import {loadNotes, onNewNote, onSelected} from './socket.js';
+import {onHandleSubmit, renderNotes, appendNote,fillForm} from './ui.js';
 
 
 function Note() {
+    
 
+    useEffect(() => {
+        onNewNote(appendNote);
+        loadNotes(renderNotes);
+        onSelected(fillForm);
+        // Form notes
+        const noteForm = document.querySelector('#noteForm')
+        noteForm.addEventListener('submit', onHandleSubmit)
 
+    },[])
 
     return (
     <div>
-        <form id="noteForm">
-            <label for="title" type="text">Title</label>
-            <input type="text" id="title" name="title" /><br/><br/>
-            <textarea type="text" id="description" rows="4"></textarea>
-            <button>Send</button>
-        </form>
-        <ul id="notes"></ul>
-
+        
     </div>
     );
 }
